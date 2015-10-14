04f3e338428143c1c7061302f5f524939921abbf
unnur28 <unnur28@gmail.com>
2015-10-14 13:59:54 +0000

    Added custom delimiter and improved newline delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,11 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if (text.contains("//"))
+		{
+			return sum(customSplit(text));
+		}
+		else if(text.contains(",") || text.contains("\n")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -19,6 +23,14 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    return numbers.split(",|\\n");
+	}
+	
+	private static String[] customSplit(String numbers)
+	{
+		String[] temp = new String[2];
+		temp = numbers.split("\\n");
+		String delimiter = temp[0].substring(2);
+		return temp[1].split(delimiter);
 	}
       
     private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -34,4 +34,8 @@ public class CalculatorTest {
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
 
+	@Test
+	public void testCustomDelimiter(){
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
